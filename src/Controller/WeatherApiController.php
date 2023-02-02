9f1f6836b4caff9c86d2e287a41e6451ca9eb987
jasioCode <101008688+jasiuSYN@users.noreply.github.com>
2023-02-02 23:41:01 +0100

    Add endpoints for city/coordinates

--- src/Controller/WeatherApiController.php
+++ src/Controller/WeatherApiController.php
@@ -28,44 +28,10 @@ class WeatherApiController extends AbstractController
         return $response->toArray();
 
     }
-    public function fetchLonLat(string $city): JsonResponse
+
+    public function getWeatherArrayContext(array $data): array
     {
-
-        $url = sprintf(
-            "https://api.openweathermap.org/geo/1.0/direct?q=%s&limit=1&appid=%s",
-            ucwords($city),
-            self::APIKEY);
-
-        $data = $this->getContentFromUrl($url);
-
-        if (!empty($data))
-            $coordinates = array('lat'=> $data[0]['lat'], 'lon' => $data[0]['lon']);
-        else
-            return new JsonResponse(
-                ['code' => Response::HTTP_BAD_REQUEST, 'message' => 'City not found'],
-                Response::HTTP_BAD_REQUEST);
-
-        return new JsonResponse($coordinates, Response::HTTP_OK);
-    }
-
-    #[Route('/api/weather', name: 'weather')]
-    public function weatherForLocation(Request $request): JsonResponse
-    {
-        $city = $request->query->get('city');
-
-        $content = $this->fetchLonLat($city)->getContent();
-
-        $coordinates = json_decode($content, true);
-
-        $url = sprintf(
-            'https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=%s&units=metric',
-            $coordinates['lat'], $coordinates['lon'], self::APIKEY
-        );
-
-        $data = $this->getContentFromUrl($url);
-
         $weatherData = [
-            'city' => ucwords($city),
             'weather' => [
                 'main' => $data['weather'][0]['main'],
                 'description' => $data['weather'][0]['description']
@@ -78,6 +44,43 @@ class WeatherApiController extends AbstractController
             'pressure' => $data['main']['pressure'],
             'humidity' => $data['main']['humidity'],
         ];
+        return $weatherData;
+    }
+    #[Route('/api/weather', name: 'lonLat')]
+    public function weatherForLonLat(Request $request): JsonResponse
+    {
+        $lat = $request->query->get('lat');
+        $lon = $request->query->get('lon');
+
+        $url = sprintf(
+            "https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=%s&units=metric",
+            $lat, $lon, self::APIKEY);
+
+        $data = $this->getContentFromUrl($url);
+
+        if (!empty($data))
+            $weatherData = $this->getWeatherArrayContext($data);
+        else
+            return new JsonResponse(
+                ['code' => Response::HTTP_BAD_REQUEST, 'message' => 'City not found'],
+                Response::HTTP_BAD_REQUEST);
+
+        return new JsonResponse($weatherData, Response::HTTP_OK);
+    }
+
+    #[Route('/api/weather/location', name: 'city')]
+    public function weatherForLocation(Request $request): JsonResponse
+    {
+        $city = $request->query->get('city');
+
+        $url = sprintf(
+            'https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=metric',
+            $city, self::APIKEY
+        );
+
+        $data = $this->getContentFromUrl($url);
+
+        $weatherData = $this->getWeatherArrayContext($data);
 
         return new JsonResponse($weatherData, Response::HTTP_OK);
     }
