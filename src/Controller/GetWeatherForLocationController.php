3aa5c864e02ec32189ff876455364b06fb0a5317
jasioCode <101008688+jasiuSYN@users.noreply.github.com>
2023-02-08 20:29:23 +0100

    Clean code

--- src/Controller/GetWeatherForLocationController.php
+++ src/Controller/GetWeatherForLocationController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 
 use App\Model\Coordinates;
 use App\Util\OpenWeatherMapClient;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,22 +16,21 @@ use Symfony\Component\Serializer\SerializerInterface;
 
 class GetWeatherForLocationController extends AbstractController
 {
-    public function __construct(private OpenWeatherMapClient $client)
-    {
-    }
+    public function __construct(private OpenWeatherMapClient $client) {}
 
-    #[Route('/api/location', name: 'location')]
-    public function __invoke(Request $request, SerializerInterface $serializer): JsonResponse
+    #[Route('/api/weather', name: 'weather')]
+    public function __invoke(Request $request, SerializerInterface $serializer): Response
     {
         $data = $request->query->all();
 
         $jsonData = json_encode($data);
+
         $coordinates = $serializer->deserialize($jsonData, Coordinates::class, 'json');
 
         $weatherData = $this->client->fetchWeatherForCoordinates($coordinates);
 
         $jsonWeatherData = $serializer->serialize($weatherData, 'json');
 
-        return new JsonResponse($jsonWeatherData, 200);
+        return new JsonResponse($jsonWeatherData, 200, [], true);
     }
 }
