34d10b41dd812882ed9f3934fdbffe4cf8ada601
jasioCode <101008688+jasiuSYN@users.noreply.github.com>
2023-02-08 16:43:01 +0100

    Add serializer

--- src/Controller/GetWeatherForLocationController.php
+++ src/Controller/GetWeatherForLocationController.php
@@ -11,59 +11,26 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class GetWeatherForLocationController extends AbstractController
 {
-
-    public function __construct(private OpenWeatherMapClient $client) {}
-
-    #[Route('/api/location', name: 'location')]
-    public function __invoke(Request $request): JsonResponse
+    public function __construct(private OpenWeatherMapClient $client)
     {
-        $latitude = $request->query->get('lat');
-        $longitude = $request->query->get('lon');
-        $coordinates = new Coordinates($latitude, $longitude);
-
-        $data = $this->client->fetchWeatherForCoordinates($coordinates);
-        
-        return new JsonResponse([
-            'main' => $data->getMain(),
-            'description' => $data->getDescription(),
-            'temp' => $data->getTempAvg(),
-        ],
-        Response::HTTP_OK);
     }
 
+    #[Route('/api/location', name: 'location')]
+    public function __invoke(Request $request, SerializerInterface $serializer): JsonResponse
+    {
+        $data = $request->query->all();
 
-//    private function getContentFromUrl(string $url): array
-//    {
-//        if ($this->client->request('GET', $url)->getStatusCode() < 400){
-//
-//            $response = $this->client->request('GET', $url);
-//            return $response->toArray();
-//        }
-//
-//        else{
-//            return ['cod' => Response::HTTP_BAD_REQUEST, 'message' => 'Wrong data'];
-//        }
-//    }
+        $jsonData = json_encode($data);
+        $coordinates = $serializer->deserialize($jsonData, Coordinates::class, 'json');
 
-//    private function getWeatherArrayContext(array $data): array
-//    {
-//        $weatherData = [
-//            'weather' => [
-//                'main' => $data['weather'][0]['main'],
-//                'description' => $data['weather'][0]['description']
-//            ],
-//            'temp' => [
-//                'temp_avg' => $data['main']['temp'],
-//                'temp_min' => $data['main']['temp_min'],
-//                'temp_max' => $data['main']['temp_max']
-//            ],
-//            'pressure' => $data['main']['pressure'],
-//            'humidity' => $data['main']['humidity'],
-//        ];
-//        return $weatherData;
-//    }
+        $weatherData = $this->client->fetchWeatherForCoordinates($coordinates);
+
+        $jsonWeatherData = $serializer->serialize($weatherData, 'json');
+
+        return new JsonResponse($jsonWeatherData, 200);
+    }
 }
