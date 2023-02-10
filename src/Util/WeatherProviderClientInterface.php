42cbc70223ac7fb3db191b89a5d90662236965da
jasioCode <101008688+jasiuSYN@users.noreply.github.com>
2023-02-10 20:10:32 +0100

    Delete function fetchWeatherForCityName

--- src/Util/WeatherProviderClientInterface.php
+++ src/Util/WeatherProviderClientInterface.php
@@ -3,12 +3,10 @@
 namespace App\Util;
 
 use App\Model\Coordinates;
-use App\Model\Localization;
 use App\Model\WeatherData;
 
 interface WeatherProviderClientInterface
 {
     public function fetchWeatherForCoordinates(Coordinates $coordinates): WeatherData;
 
-    public function fetchWeatherForCityName(Localization $localization): WeatherData;
 }
