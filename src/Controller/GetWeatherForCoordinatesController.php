4fb348b909765b006129881640d7ce48e796f98c
jasioCode <101008688+jasiuSYN@users.noreply.github.com>
2023-02-19 12:52:50 +0100

    Add Tomorrow API model

--- src/Controller/GetWeatherForCoordinatesController.php
+++ src/Controller/GetWeatherForCoordinatesController.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Client\Weather\OpenWeatherMap\Client;
+//use App\Client\Weather\OpenWeatherMap\Client;
+use App\Client\Weather\Tomorrow\Client;
+use App\Client\Weather\WeatherProviderClientInterface;
 use App\Model\Coordinates;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -15,14 +17,14 @@ use Symfony\Component\Serializer\SerializerInterface;
 
 class GetWeatherForCoordinatesController extends AbstractController
 {
-    public function __construct(private Client $client) {}
+    public function __construct(private WeatherProviderClientInterface $openWeatherClient) {}
 
     #[Route('/api/weather-by-coordinates', name: 'weather-coordinates')]
     public function __invoke(Request $request, SerializerInterface $serializer, ObjectNormalizer $normalizer): JsonResponse
     {
         $coordinates = $normalizer->denormalize($request->query->all(), Coordinates::class);
 
-        $weatherData = $this->client->fetchWeatherForCoordinates($coordinates);
+        $weatherData = $this->openWeatherClient->fetchWeatherForCoordinates($coordinates);
 
         return JsonResponse::fromJsonString($serializer->serialize($weatherData, 'json'));
     }
