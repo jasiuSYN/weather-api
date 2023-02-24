8b5e76b5900a6daf06d81df87c3c5523be4fbfd3
jasioCode <101008688+jasiuSYN@users.noreply.github.com>
2023-02-24 10:33:43 +0100

    Refactor error message

--- src/Model/Coordinates.php
+++ src/Model/Coordinates.php
@@ -9,21 +9,21 @@ use Symfony\Component\Validator\Constraints as Assert;
 class Coordinates
 {
     #[Assert\NotBlank(
-        message: 'WEATHER_REQUEST_PARAMETER_IS_EMPTY'
+        message: 'WEATHER_REQUEST_LATITUDE_IS_EMPTY'
     )]
     #[Assert\Range(
         notInRangeMessage: 'WEATHER_REQUEST_LATITUDE_IS_OUT_OF_RANGE',
-        invalidMessage: 'WEATHER_REQUEST_PARAMETER_IS_NOT_VALID_NUMBER',
+        invalidMessage: 'WEATHER_REQUEST_INVALID_LATITUDE_VALUE',
         min: -90,
         max: 90
     )]
     private string $latitude;
     #[Assert\NotBlank(
-        message: 'WEATHER_REQUEST_PARAMETER_IS_EMPTY'
+        message: 'WEATHER_REQUEST_LONGITUDE_IS_EMPTY'
     )]
     #[Assert\Range(
         notInRangeMessage: 'WEATHER_REQUEST_LONGITUDE_IS_OUT_OF_RANGE',
-        invalidMessage: 'WEATHER_REQUEST_PARAMETER_IS_NOT_VALID_NUMBER',
+        invalidMessage: 'WEATHER_REQUEST_INVALID_LONGITUDE_VALUE',
         min: -180,
         max: 180
     )]
