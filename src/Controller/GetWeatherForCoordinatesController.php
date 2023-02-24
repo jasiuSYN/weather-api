ac30912c9fb78a34c3f83542f62cf5727fa1b882
jasioCode <101008688+jasiuSYN@users.noreply.github.com>
2023-02-24 10:38:34 +0100

    Refactor for readability

--- src/Controller/GetWeatherForCoordinatesController.php
+++ src/Controller/GetWeatherForCoordinatesController.php
@@ -7,7 +7,7 @@ namespace App\Controller;
 use App\Client\Weather\WeatherProviderClientInterface;
 use App\Model\Coordinates;
 use App\Model\Errors\Error;
-use App\Model\Errors\ErrorList;
+use App\Model\Errors\ErrorsList;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,29 +18,40 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class GetWeatherForCoordinatesController extends AbstractController
 {
-    public function __construct(private WeatherProviderClientInterface $client) {}
+    public function __construct(
+        private SerializerInterface $serializer,
+        private ObjectNormalizer $normalizer,
+        private ValidatorInterface $validator,
+        private WeatherProviderClientInterface $client) {}
 
     #[Route('/api/weather-by-coordinates', name: 'weather-coordinates')]
-    public function __invoke(Request $request, SerializerInterface $serializer, ObjectNormalizer $normalizer, ValidatorInterface $validator): JsonResponse
+    public function __invoke(Request $request): JsonResponse
     {
-        $coordinates = $normalizer->denormalize($request->query->all(), Coordinates::class);
+        $coordinates = $this->normalizer->denormalize($request->query->all(), Coordinates::class);
 
-        $errors = $validator->validate($coordinates);
+        $errors = $this->validator->validate($coordinates);
 
         if ($errors->count() > 0) {
 
-            $errorList = new ErrorList();
+            $errorList = new ErrorsList();
 
             foreach ($errors as $error) {
 
-                $errorList->addError(new Error($error->getCode(), $error->getMessage()));
+                $errorList->addError(
+                    new Error($error->getMessage(),
+                        "",
+                        $error->getPropertyPath())
+                );
             }
 
-            return JsonResponse::fromJsonString($serializer->serialize($errorList, 'json'), 400);
+            return JsonResponse::fromJsonString(
+                $this->serializer->serialize($errorList, 'json'),
+                $errorList->getHttpStatusCode()
+            );
         }
 
         $weatherData = $this->client->fetchWeatherForCoordinates($coordinates);
 
-        return JsonResponse::fromJsonString($serializer->serialize($weatherData, 'json'));
+        return JsonResponse::fromJsonString($this->serializer->serialize($weatherData, 'json'));
     }
 }
